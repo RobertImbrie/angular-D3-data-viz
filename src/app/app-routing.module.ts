508f18811589adb999f9dd36431ce86d06d6393a
RobertImbrie <robert.m.imbrie@gmail.com>
2018-05-26 13:45:19 -0500

    Adding form component and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './core/home/home.component';
 import { DataDisplayComponent } from './data-display/data-display.component';
+import { FormComponent } from './form/form.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'form', component: FormComponent },
     { path: 'data', component: DataDisplayComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './core/home/home.component';
 import { HeaderComponent } from './core/header/header.component';
 import { DataDisplayComponent } from './data-display/data-display.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    DataDisplayComponent
+    DataDisplayComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
