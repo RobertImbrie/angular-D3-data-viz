cc9a0c3dfd54d292d2554e4bfc12a2611b1ebce9
RobertImbrie <robert.m.imbrie@gmail.com>
2018-05-28 14:48:01 -0500

    The bar chart is dynamic! Woo! Now I just need to figure out the axes

--- src/app/data-display/bar-chart.directive.ts
+++ src/app/data-display/bar-chart.directive.ts
@@ -7,10 +7,14 @@ import { D3Service, D3 } from 'd3-ng2-service';
 })
 export class BarChartDirective implements OnInit {
   @Input() data: number[];
+  @Input() height    = 200;
+  @Input() width     = 200;
   @Input() barColor  = 'black';
   @Input() textColor = 'white';
-  @Input() margin    = '1px';
-  
+  @Input() margin    = 1;
+  @Input() xAxis     = 'X-Axis';
+  @Input() yAxis     = 'Y-Axis';
+
   private d3: D3;
   private parentNativeElement: any;
 
@@ -26,10 +30,10 @@ export class BarChartDirective implements OnInit {
   ngOnInit() {
     const d3 = this.d3;
     let d3ParentElement: Selection<any, any, any, any>;
-    console.log(this.data);
+    console.log(this.barColor);
     const x = d3.scaleLinear()
       .domain([0, d3.max(this.data)])
-      .range([0, 420]);
+      .range([0, this.width]);
 
     if (this.parentNativeElement !== null) {
       d3ParentElement = d3.select(this.parentNativeElement)
@@ -37,8 +41,12 @@ export class BarChartDirective implements OnInit {
           .data(this.data)
         .enter().append('div')
           .style('width', function(d) { return x(d) + 'px'; })
-          .style('background-color', 'blue')
-          .style('margin', '1px')
+          .style('height', (this.height / this.data.length - this.margin) + 'px')
+          .style('background-color', this.barColor)
+          .style('margin', this.margin + 'px')
+          .style('padding-right', '10px')
+          .style('color', this.textColor)
+          .style('text-align', 'right')
           .text(function(d) { return d; });
     }
   }
